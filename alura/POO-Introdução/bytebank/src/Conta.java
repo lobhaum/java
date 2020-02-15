518c48a34181f6e1eaed24cb9c95b1dde4230e05
lobhaum <contato@eduardoluciano.com.br>
2020-02-15 18:12:42 -0300

    Add classes files

--- alura/POO-Introdução/bytebank/src/Conta.java
+++ alura/POO-Introdução/bytebank/src/Conta.java
@@ -1,11 +1,39 @@
-public class Conta{
-	
-	
+public class Conta {
+
 	// atributos: (propriedades)
 	double saldo;
-	int agencia = 993;
+	int agencia;
 	int numero;
 	String titular;
+
+	// métodos:
+	public void deposita(double valor) { // void não devolve nada como resposta
+		this.saldo += valor;
+		// this é a conta qual eu estou invocando
+		// saldo é atributo e valor é variavel que recebe o valor
+
+	}
+
+	public boolean saca(double valor) {
+
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			// este saldo fica valendo o saldo antigo menos o valor que deseja sacar
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
-	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);
+			return true;
+		}else {
+			System.out.println("Saldo insuficiente");
+			return false;
+		}
+		
+	}
 }
---
+++ alura/POO-Introdução/bytebank/src/TestaMetodo.java
@@ -0,0 +1,19 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		boolean conseguiuRetirar = contaDoPaulo.saca(10); // armazena o retorno do metodo na variavel
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar); // exibe retorno do metodo armazenado na variavel
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		contaDaMarcela.transfere(300, contaDoPaulo);
+		System.out.println("Transferencia concluida");
+		System.out.println("Saldo atual: " + contaDaMarcela.saldo);
+		System.out.println("Saldo atual: " + contaDoPaulo.saldo);
+	}
+}
