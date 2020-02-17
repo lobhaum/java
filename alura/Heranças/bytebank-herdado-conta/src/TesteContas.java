4ba519ac990a8808805feba6c1f5dfe2c2687cbb
lobhaum <contato@eduardoluciano.com.br>
2020-02-17 11:05:21 -0300

    Add classes files

---
+++ alura/Heranças/bytebank-herdado-conta/src/TesteContas.java
@@ -0,0 +1,17 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente contaCorrente = new ContaCorrente(2978, 7651);
+		ContaPoupanca contaPoupanca = new ContaPoupanca(2978, 76510);
+		
+		contaCorrente.deposita(1500.0);
+		contaPoupanca.deposita(10500.29);
+		System.out.println(contaCorrente.getSaldo());
+		
+		contaCorrente.transfere(1500.0, contaPoupanca);
+		System.out.println("Saldo Conta Corrente: " + contaCorrente.getSaldo());
+		System.out.println("Saldo Conta Poupança: " + contaPoupanca.getSaldo());
+	}
+}
