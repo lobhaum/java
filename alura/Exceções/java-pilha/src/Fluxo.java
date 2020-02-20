7e1f0196a2edaa8f1087ab4b14a42db0bdafb605
lobhaum <contato@eduardoluciano.com.br>
2020-02-20 08:41:06 -0300

    Add classes files

--- alura/Exceções/java-pilha/src/Fluxo.java
+++ alura/Exceções/java-pilha/src/Fluxo.java
@@ -2,13 +2,29 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+			metodo1();
+		} catch (ArithmeticException e) {
+			// TODO: handle exception
+			//System.out.println("ArithmeticException");
+			//System.out.println(e);
+			String msg = e.getMessage();
+			System.out.println("ArithmeticException " + msg);
+			System.out.println("printStackTrace:");
+			e.printStackTrace();
+		} catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+        
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+
+        	metodo2();
+
+        
         System.out.println("Fim do metodo1");
     }
 
@@ -16,6 +32,11 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            // int a = i / 0;
+            Conta c = null;
+            c.deposita();
+            
+            
         }
         System.out.println("Fim do metodo2");
     }
