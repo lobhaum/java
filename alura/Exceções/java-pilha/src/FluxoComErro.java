727e4c8356f2933dbc38eecf70289e572492ce34
lobhaum <contato@eduardoluciano.com.br>
2020-02-20 10:30:53 -0300

    Add classes files

--- alura/Exceções/java-pilha/src/FluxoComErro.java
+++ alura/Exceções/java-pilha/src/FluxoComErro.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComErro {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -17,7 +17,7 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao{
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
 
         	metodo2();
@@ -26,9 +26,9 @@ public class Fluxo {
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao{
-        System.out.println("Ini do metodo2");
-        throw new MinhaExcecao("Deu errado");
-        //System.out.println("Fim do metodo2");
+    private static void metodo2() {
+        System.out.println("ini metodo 2");
+    	metodo2();
+    	System.out.println("fim metodo 2");
     }
 }
