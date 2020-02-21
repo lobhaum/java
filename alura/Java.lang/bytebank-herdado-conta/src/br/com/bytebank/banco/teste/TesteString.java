ba4d407bcf5512ee37b4b026517164d47ace74a8
lobhaum <contato@eduardoluciano.com.br>
2020-02-21 10:01:20 -0300

    Add classes files

---
+++ alura/Java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,15 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "alura"; // object literal
+		System.out.println(nome);
+		String troca = nome.replace("a", "A");
+		System.out.println(troca);
+		System.out.println(nome.toLowerCase());
+
+	}
+
+}
