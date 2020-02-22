3fe9a0589c2857591572f1d5f0d7199b12e781dc
lobhaum <contato@eduardoluciano.com.br>
2020-02-22 17:18:58 -0300

    Add classes files

--- alura/Java.io/java-io/src/br/com/alura/java/io/teste/TesteLeituraCSV.java
+++ alura/Java.io/java-io/src/br/com/alura/java/io/teste/TesteLeituraCSV.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraCSV {
@@ -14,7 +15,26 @@ public class TesteLeituraCSV {
 		
 		while(temLinha) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+			//System.out.println(linha);
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String cliente = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			System.out.println("Agencia: " + agencia + " " 
+					+ tipoConta + ": " +  numero + " " +
+					cliente + " R$ " + saldo );
+			
+			linhaScanner.close();
+			
 		}
 		scanner.close();
 		
