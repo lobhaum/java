0b7a4711dc7c6908d9e5b25f214d5492f8350d88
lobhaum <contato@eduardoluciano.com.br>
2020-02-22 16:28:16 -0300

    Add classes files

--- alura/Java.io/java-io/src/br/com/alura/java/io/teste/TesteEscrita3.java
+++ alura/Java.io/java-io/src/br/com/alura/java/io/teste/TesteEscrita3.java
@@ -1,8 +1,8 @@
 package br.com.alura.java.io.teste;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class TesteEscrita3 {
 
@@ -14,19 +14,17 @@ public class TesteEscrita3 {
 //		Writer osw = new OutputStreamWriter(fos);
 //		BufferedWriter bw = new BufferedWriter(osw);
 
-		
-		FileWriter fw = new FileWriter("lorem-fileWriter.txt");
-		BufferedWriter bw = new BufferedWriter(fw);
-		
-		bw.write("Leva algum tempo para você "
-				+ "realmente assimilar que você é bom o suficiente.");
-		bw.write(System.lineSeparator());
-		bw.write(System.lineSeparator());
-		bw.write(System.lineSeparator());
-		bw.write("Eu acredito em você e na sua capacidade porque conheço "
-				+ "o seu esforço e a sua motivação. Não desista!");
-		
-		bw.close();
+//		FileWriter fw = new FileWriter("lorem-fileWriter.txt");
+//		BufferedWriter bw = new BufferedWriter(fw);
+
+		// Imprime para um fluxo binário:
+		PrintStream printStream = new PrintStream(new File("lorem-printStream.txt"));
+
+		printStream.println("Leva algum tempo para você " + "realmente assimilar que você é bom o suficiente.");
+		printStream.println();
+		printStream.println("Fim");
+
+		printStream.close();
 
 	}
 
