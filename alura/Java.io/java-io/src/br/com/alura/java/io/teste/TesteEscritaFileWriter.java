357386a2fb504d2797128e7ddb312a3e8d6bd1d4
lobhaum <contato@eduardoluciano.com.br>
2020-02-22 14:59:32 -0300

    Add classes files

---
+++ alura/Java.io/java-io/src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
@@ -0,0 +1,33 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TesteEscritaFileWriter {
+
+	public static void main(String[] args) throws IOException {
+
+		// Fluxo de entrada com arquivo
+
+//		OutputStream fos = new FileOutputStream("lorem-gravacao.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+
+		
+		FileWriter fw = new FileWriter("lorem-fileWriter.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write("Leva algum tempo para você "
+				+ "realmente assimilar que você é bom o suficiente.");
+		bw.write(System.lineSeparator());
+		bw.write(System.lineSeparator());
+		bw.write(System.lineSeparator());
+		bw.write("Eu acredito em você e na sua capacidade porque conheço "
+				+ "o seu esforço e a sua motivação. Não desista!");
+		
+		bw.close();
+
+	}
+
+}
