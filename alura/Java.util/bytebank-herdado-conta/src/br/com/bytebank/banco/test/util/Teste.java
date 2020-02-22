e2fb77e2e7622a334b2176e89878afbcf6811cf7
lobhaum <contato@eduardoluciano.com.br>
2020-02-21 22:34:59 -0300

    Add classes files

---
+++ alura/Java.util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -0,0 +1,46 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		ArrayList lista = new ArrayList();
+		
+		Conta cc1 = new ContaCorrente(222, 2345);
+		lista.add(cc1);
+		
+		Conta cc2 = new ContaCorrente(222, 2346);
+		lista.add(cc2);
+		
+		Conta cc3 = new ContaCorrente(222, 2347);
+		lista.add(cc3);
+		
+		Conta cc4 = new ContaCorrente(222, 2348);
+		lista.add(cc4);
+		
+		System.out.println(lista.size());
+		
+		Conta referencia = (Conta) lista.get(0);
+		System.out.println(referencia);
+		
+		
+		lista.remove(0);
+		System.out.println("Tamanho: " + lista.size());
+		
+//		for (int i = 0; i < lista.size(); i++) {
+//			Object oRef = lista.get(i);
+//			System.out.println(oRef);
+//		}
+		
+		for (Object oRef : lista) {
+			System.out.println(oRef);
+		}
+		
+	}
+
+}
