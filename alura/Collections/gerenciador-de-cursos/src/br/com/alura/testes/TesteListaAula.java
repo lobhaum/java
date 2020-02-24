14a105e92a8a0060c7d2bb5154a75bd6c424c3de
lobhaum <contato@eduardoluciano.com.br>
2020-02-24 19:01:47 -0300

    Criado os objetos aulas?? e adicionados ao ArrayList<Aula>

--- alura/Collections/gerenciador-de-cursos/src/br/com/alura/testes/TesteListaAula.java
+++ alura/Collections/gerenciador-de-cursos/src/br/com/alura/testes/TesteListaAula.java
@@ -1,10 +1,23 @@
 package br.com.alura.testes;
 
+import java.util.ArrayList;
+
+import br.com.alura.Aula;
+
 public class TesteListaAula {
 
 	public static void main(String[] args) {
+		// Instanciando os objetos aula?? e alimentando o construtor:
+		Aula aula01 = new Aula("Revistando as ArralyLists", 21);
+		Aula aula02 = new Aula("lista de objetos", 15);
+		Aula aula03 = new Aula("Relacionamento de listas e objetos", 15);
 		
-
+		
+		//Instanciando Arraylist aulas e adicionando objetos aulas??:
+		ArrayList<Aula> aulas = new ArrayList<Aula>();
+		aulas.add(aula01);
+		aulas.add(aula02);
+		aulas.add(aula03);
 	}
 
 }
