4072f23c3f032093531c49a3820542edc9d6c67f
lobhaum <contato@eduardoluciano.com.br>
2020-02-25 09:11:36 -0300

    Criação da classe Aluno e construtor

---
+++ alura/Collections/gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -0,0 +1,15 @@
+package br.com.alura;
+
+public class Aluno {
+	
+	private String nome;
+	private int numeroMatricula;
+	
+	//Criando o construtor:
+	public Aluno(String nome, int numeroMatricula) {
+		
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+}
