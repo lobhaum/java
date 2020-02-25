b21ec6676b20e12c3fea5e5ac562f389cf7dd738
lobhaum <contato@eduardoluciano.com.br>
2020-02-25 14:06:12 -0300

    iniciando projeto IMC

---
+++ IMC/IMC/src/br/com/lobhaum/main/IMC.java
@@ -0,0 +1,20 @@
+package br.com.lobhaum.main;
+
+public class IMC {
+	
+	// Declarando atributos com valores de entrada
+	private double altura 	= 1.7;
+	private double kilo		= 70.0;
+	
+	/**
+	 * Métodos:
+	 */
+	// getters:
+	public double getAltura() {
+		return altura;
+	}
+	public double getKilo() {
+		return kilo;
+	}
+
+}
