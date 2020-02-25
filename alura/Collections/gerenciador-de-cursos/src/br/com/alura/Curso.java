daa5a6fcc4f68fae67903014162f826a0584f550
lobhaum <contato@eduardoluciano.com.br>
2020-02-25 07:23:54 -0300

    Criação do constructor

--- alura/Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ alura/Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,11 +1,22 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
-	private ArrayList<Aula> aulas;
+	private List<Aula> aulas = new ArrayList<Aula>(); // list ao inves de ArrayList encapsula melhor
+
+	// Criação do constructor:
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 	
+	// Criação dos getters:
+	public List<Aula> getAulas() {
+		return aulas; 
+	}
 }
