1e9a15abcc9b6052f7ee3e37d40ec88748ba4e6c
lobhaum <contato@eduardoluciano.com.br>
2020-02-26 10:02:27 -0300

    reconfigurado jetty

--- alura/Maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
+++ alura/Maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -1,7 +1,26 @@
 package br.com.alura.maven.lojaweb;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/contato"})
 public class ContatoServlet extends HttpServlet{
 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { 
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><h2>Entre em Contato via email</h2></html>");
+        writer.close();
+    }
 }
