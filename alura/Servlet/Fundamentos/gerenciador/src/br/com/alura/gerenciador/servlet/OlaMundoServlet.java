97b8d36b8a290d41b83c41c53005e7d266f11ef2
lobhaum <contato@eduardoluciano.com.br>
2020-02-26 16:10:54 -0300

    sysout no servlet

---
+++ alura/Servlet/Fundamentos/gerenciador/src/br/com/alura/gerenciador/servlet/OlaMundoServlet.java
@@ -0,0 +1,23 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/ola-mundo"})
+public class OlaMundoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body><p>Ola Mundo do Servlet!!!</p></body></html>");
+		System.out.println("Servlet OlaMundoServlet foi chamado");
+		out.close();
+	}
+}
