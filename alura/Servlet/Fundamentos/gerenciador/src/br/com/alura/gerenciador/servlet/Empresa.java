beb89565df7a00e1cfb36acd5ff362deb06fef9b
lobhaum <contato@eduardoluciano.com.br>
2020-02-28 09:32:40 -0300

    created getters and setter of id and nome

--- alura/Servlet/Fundamentos/gerenciador/src/br/com/alura/gerenciador/servlet/Empresa.java
+++ alura/Servlet/Fundamentos/gerenciador/src/br/com/alura/gerenciador/servlet/Empresa.java
@@ -2,7 +2,20 @@ package br.com.alura.gerenciador.servlet;
 
 public class Empresa {
 	
+	private int id;
 	private String nome;
 	
+	public int getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
