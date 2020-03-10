5ed47879f5e1156589b7380a430967644c2c143a
lobhaum <contato@eduardoluciano.com.br>
2020-03-10 19:18:37 -0300

    trocado o getRequestDispatcher para /listaEmpresa.jsp

--- alura/Servlet/Fundamentos/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ alura/Servlet/Fundamentos/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -35,7 +35,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		banco.adiciona(empresa);
 
 		// chamar o JSP:
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresa.jsp");
 		request.setAttribute("empresa", empresa.getNome());
 		rd.forward(request, response);
 	}
