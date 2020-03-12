c82e41b38e01e2a9a59afdc04784136a37fd543f
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 20:14:42 -0300

    Criando conexão e conectando ao BD

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
@@ -1,9 +1,14 @@
 package lojaVirtual;
 
+import java.sql.Connection;
+
 public class TestaInsercaoComParametros {
 
 	public static void main(String[] args) {
 
+		ConnectionFactory factory = new ConnectionFactory(); // Criando objeto de conexão
+		Connection con = factory.recuperaConexao(); // conectando ao banco de dados
+		
 	}
 
 }
