17c9ca39eec121ac7aa0d411e50987f1c6ec2b2b
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 20:01:40 -0300

    Nesta aula, aprendemos que:
    Para simplificar e encapsular a criação da conexão, devemos usar uma
    classe ConnectionFactory
    A classe ConnectionFactory segue o padrão de criação Factory Method
    O Factory Method encapsula a criação de um objeto
    Para executar um comando SQL, podemos usar a interface
    java.sql.Statement
    O método execute envia o comando para o banco de dados
    Dependendo do comando SQL, podemos recuperar a chave primária ou os
    registros selecionados

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaRemocao.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaRemocao.java
@@ -18,7 +18,16 @@ public class TestaRemocao {
 		
 		System.out.println(linhasModificadas);
 		
-		
+		/**
+		 * Nesta aula, aprendemos que:
+
+Para simplificar e encapsular a criação da conexão, devemos usar uma classe ConnectionFactory
+A classe ConnectionFactory segue o padrão de criação Factory Method
+O Factory Method encapsula a criação de um objeto
+Para executar um comando SQL, podemos usar a interface java.sql.Statement
+O método execute envia o comando para o banco de dados
+Dependendo do comando SQL, podemos recuperar a chave primária ou os registros selecionados
+		 */
 		
 				
 	}
