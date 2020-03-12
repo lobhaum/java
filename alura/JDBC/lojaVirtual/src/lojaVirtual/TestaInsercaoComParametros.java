2dcbecd6c07bb4bbf9ddc09661a5d3a7fd3dd0f6
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 20:15:45 -0300

    refadorado factory para cf e adicionado a exceção SQL

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
@@ -1,13 +1,14 @@
 package lojaVirtual;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TestaInsercaoComParametros {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 
-		ConnectionFactory factory = new ConnectionFactory(); // Criando objeto de conexão
-		Connection con = factory.recuperaConexao(); // conectando ao banco de dados
+		ConnectionFactory cf = new ConnectionFactory(); // Criando objeto de conexão
+		Connection con = cf.recuperaConexao(); // conectando ao banco de dados
 		
 	}
 
