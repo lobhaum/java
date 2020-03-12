01409f40f60434c0152187b49140d7a1b244d43f
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 20:08:09 -0300

    Comentado o stm.execute Testando inserção sem o ",Statement.RETURN_GENERATED_KEY - Método necessário para a persistencia

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercao.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercao.java
@@ -16,11 +16,14 @@ public class TestaInsercao {
 		// metodo execute retorna true quando é lista
 		// metodo execute insert retorna false
 		//boolean resultado = 
-		stm.execute("INSERT INTO produto (nome, descricao) VALUES ('ssd', 'SSD Kingston A400, 240GB'),"
-				+ "('notebook','Acer Aspire 5 i5-8265U 8GB, 1TB, SSD 128GB'),"
-				+ "('videogame','Microsoft Xbox One S 1TB'),"
-				+ "('mainboard','Gigabyte Z390 M Gaming LGA 1151 DDR4');"
-				, Statement.RETURN_GENERATED_KEYS);
+//		stm.execute("INSERT INTO produto (nome, descricao) VALUES ('ssd', 'SSD Kingston A400, 240GB'),"
+//				+ "('notebook','Acer Aspire 5 i5-8265U 8GB, 1TB, SSD 128GB'),"
+//				+ "('videogame','Microsoft Xbox One S 1TB'),"
+//				+ "('mainboard','Gigabyte Z390 M Gaming LGA 1151 DDR4');"
+//				, Statement.RETURN_GENERATED_KEYS);
+		stm.execute("INSERT INTO produto (nome, descricao) VALUES ('headset', 'Headset Gamer Razer Electra V2 7.1 Virtual - USB'),"
+				+ "('teclado','Teclado Mecânico Gamer Razer Blackwindow Tournament V2 Chroma');"
+				,Statement.RETURN_GENERATED_KEYS);
 		ResultSet rst = stm.getGeneratedKeys();
 		while(rst.next()) {
 			Integer id = rst.getInt(1);
