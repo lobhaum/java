1e001e84044601736542850047f10e28a5e3f298
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 20:56:37 -0300

    PreparedStatement implementado com sucesso

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
@@ -1,6 +1,7 @@
 package lojaVirtual;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,18 +14,24 @@ public class TestaInsercaoComParametros {
 
 		ConnectionFactory cf = new ConnectionFactory(); // Criando objeto de conexão
 		Connection con = cf.recuperaConexao(); // conectando ao banco de dados
-		Statement stm = con.createStatement(); // cria um estado para enviar requisições ao banco de dados
-		nome = "antivirus";
-		descricao = "Kaspersky Security Cloud";
-		
+		// Usando Prepared Statement:
+		// Statement stm = con.createStatement(); // cria um estado para enviar
+		// requisições ao banco de dados
+
+		nome = "processador";
+		descricao = "AMD Ryzen 7 2700X, Cooler Wraith Prism, Cache 20MB";
+
 //		String sql = "INSERT INTO produto (nome, descricao) VALUES" 
 //				+ "('" + nome + "','" + descricao + "')"; 
-		
+
 		// Usando PreparedStatement:
-		String sql = "INSERT INTO produto (nome, descricao) VALUES"
-				+ "(?,?);";
-		stm.execute(sql,Statement.RETURN_GENERATED_KEYS);
-		stm.close();
+		String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?);";
+		System.out.println(sql);
+		PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		pstmt.setString(1, nome);
+		pstmt.setString(2, descricao);
+		pstmt.execute();
+		pstmt.close();
 	}
 
 }
