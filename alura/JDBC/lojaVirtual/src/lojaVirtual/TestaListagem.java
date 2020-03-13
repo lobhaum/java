5797b0ff906e659c53bfc8ac1f71e34ffc4f240e
lobhaum <contato@eduardoluciano.com.br>
2020-03-12 21:20:39 -0300

    refatorado para usar PreparedStatement

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaListagem.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaListagem.java
@@ -1,27 +1,30 @@
 package lojaVirtual;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		
 		Connection connection = connectionFactory.recuperaConexao();
 		
-		Statement stm = connection.createStatement();
-		boolean resultado = stm.execute("SELECT * FROM produto");
-		ResultSet rst = stm.getResultSet();
+		
+		String sql = "SELECT * FROM produto";
+		
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.execute();
+		// boolean resultado = pstmt.execute("SELECT * FROM produto");
+		ResultSet rst = pstmt.getResultSet();
 		while(rst.next()) {
 			Integer id = rst.getInt("id");
 			String nome = rst.getString("nome");
 			String descricao = rst.getString("descricao");
 			System.out.println("Id:" + id + " Nome: " + nome + " Descricao: " + descricao);
 		}
-		stm.close();
+		pstmt.close();
 	}
 }
