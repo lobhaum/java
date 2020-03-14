1e315b27c87fe894e249474269fb20b9921b9731
lobhaum <contato@eduardoluciano.com.br>
2020-03-14 16:15:19 -0300

    comentado os objetos nome e descricao, passando parametros diretamente no metododos adiconarVariavel

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
@@ -9,8 +9,8 @@ public class TestaInsercaoComParametros {
 
 	public static void main(String[] args) throws SQLException {
 		// instanciando os objetos string para query
-		String nome = null;
-		String descricao = null;
+//		String nome = null;
+//		String descricao = null;
 
 		ConnectionFactory cf = new ConnectionFactory(); // Criando objeto de conexão
 		Connection con = cf.recuperaConexao(); // conectando ao banco de dados
@@ -18,8 +18,8 @@ public class TestaInsercaoComParametros {
 		// Statement stm = con.createStatement(); // cria um estado para enviar
 		// requisições ao banco de dados
 
-		nome = "processador";
-		descricao = "AMD Ryzen 7 2700X, Cooler Wraith Prism, Cache 20MB";
+//		nome = "processador";
+//		descricao = "AMD Ryzen 7 2700X, Cooler Wraith Prism, Cache 20MB";
 
 //		String sql = "INSERT INTO produto (nome, descricao) VALUES" 
 //				+ "('" + nome + "','" + descricao + "')"; 
@@ -28,7 +28,8 @@ public class TestaInsercaoComParametros {
 		String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?);";
 		System.out.println(sql);
 		PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		adicionarVariavel(nome, descricao, pstmt);
+		adicionarVariavel("SmartTV", "45 polegadas", pstmt);
+		adicionarVariavel("Radio", "Radio Vintage Torcedor de Futebol", pstmt);
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement pstmt) throws SQLException {
