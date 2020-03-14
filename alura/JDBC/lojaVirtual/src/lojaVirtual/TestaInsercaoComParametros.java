6fb89983b287c5155b0ed03fc0f4671d5beefdaf
lobhaum <contato@eduardoluciano.com.br>
2020-03-14 16:25:18 -0300

    reactored comments

--- alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
+++ alura/JDBC/lojaVirtual/src/lojaVirtual/TestaInsercaoComParametros.java
@@ -8,23 +8,24 @@ import java.sql.Statement;
 public class TestaInsercaoComParametros {
 
 	public static void main(String[] args) throws SQLException {
-		// instanciando os objetos string para query
-//		String nome = null;
-//		String descricao = null;
-
+		/*
+		 * // instanciando os objetos string para query // String nome = null; // String
+		 * descricao = null;
+		 */
 		ConnectionFactory cf = new ConnectionFactory(); // Criando objeto de conexão
 		Connection con = cf.recuperaConexao(); // conectando ao banco de dados
-		// Usando Prepared Statement:
-		// Statement stm = con.createStatement(); // cria um estado para enviar
-		// requisições ao banco de dados
-
-//		nome = "processador";
-//		descricao = "AMD Ryzen 7 2700X, Cooler Wraith Prism, Cache 20MB";
-
-//		String sql = "INSERT INTO produto (nome, descricao) VALUES" 
-//				+ "('" + nome + "','" + descricao + "')"; 
-
-		// Usando PreparedStatement:
+		/*
+		 * // Usando Prepared Statement: // Statement stm = con.createStatement(); //
+		 * cria um estado para enviar // requisições ao banco de dados
+		 * 
+		 * // nome = "processador"; // descricao =
+		 * "AMD Ryzen 7 2700X, Cooler Wraith Prism, Cache 20MB";
+		 * 
+		 * // String sql = "INSERT INTO produto (nome, descricao) VALUES" // + "('" +
+		 * nome + "','" + descricao + "')";
+		 * 
+		 * // Usando PreparedStatement:
+		 */		
 		String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?);";
 		System.out.println(sql);
 		PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
