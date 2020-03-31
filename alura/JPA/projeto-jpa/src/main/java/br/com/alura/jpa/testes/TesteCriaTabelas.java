579472409db4878f39c737df27028d265a5ccdec
lobhaum <contato@eduardoluciano.com.br>
2020-03-31 09:09:04 -0300

    add

---
+++ alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteCriaTabelas.java
@@ -0,0 +1,13 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TesteCriaTabelas {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
+		EntityManager createEntityManager = entityManagerFactory.createEntityManager();
+		entityManagerFactory.close();
+	}
+}
