54ddb0e639ea68cbc09a17045d0040a32c1e30b3
lobhaum <contato@eduardoluciano.com.br>
2020-03-31 08:56:19 -0300

    criação da classe conta

---
+++ alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -0,0 +1,32 @@
+package br.com.alura.jpa.modelo;
+
+public class Conta {
+	private Integer agencia;
+	private Integer numero;
+	private String titular;
+
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+}
