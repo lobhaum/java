8eff3c6be4b49e7fc599030384ac0dd477063c7d
lobhaum <contato@eduardoluciano.com.br>
2020-04-03 07:58:38 -0300

    alterado a persistencia de contas para alura

--- alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteCriaTabelas.java
+++ alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteCriaTabelas.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class TesteCriaTabelas {
 	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("alura");
 		EntityManager createEntityManager = entityManagerFactory.createEntityManager();
 		entityManagerFactory.close();
 	}
