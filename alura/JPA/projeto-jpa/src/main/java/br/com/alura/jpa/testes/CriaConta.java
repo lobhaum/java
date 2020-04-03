6dbbd2dbd7bbd9c9fececf129636d87b957a6f0d
lobhaum <contato@eduardoluciano.com.br>
2020-04-03 07:52:37 -0300

    iniciado persistencia com EntityManager

--- alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaConta.java
+++ alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaConta.java
@@ -1,9 +1,25 @@
 package br.com.alura.jpa.testes;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
 public class CriaConta {
 
 	public static void main(String[] args) {
 
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("alura");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		
+		Conta conta = new Conta();
+		conta.setTitular("Leonardo");
+		conta.setNumero(1234);
+		conta.setAgencia(4321);
+		
+		entityManager.persist(conta);
 	}
 
 }
