a64f20596fc6f7be5b2108ccef233cd611b61436
lobhaum <contato@eduardoluciano.com.br>
2020-04-03 08:25:38 -0300

    alterado o estado do modelo e commitado com hibernate

--- alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/AlteraSaldoContaLeonardo.java
+++ alura/JPA/projeto-jpa/src/main/java/br/com/alura/jpa/testes/AlteraSaldoContaLeonardo.java
@@ -4,13 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.jpa.modelo.Conta;
+
 public class AlteraSaldoContaLeonardo {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("alura");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
+		Conta contaLeonardo = entityManager.find(Conta.class, 1L);
 		
+		System.out.println("Conta do Leonardo -> " + contaLeonardo.getTitular());
+		entityManager.getTransaction().begin();
+		contaLeonardo.setSaldo(1950.15);
+		entityManager.getTransaction().commit();
+
 	}
 
 }
